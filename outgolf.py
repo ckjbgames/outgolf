fd4b9db38a976699f0054bacaec9e354f972e753
ckjbgames <ckjbgames@gmail.com>
2017-02-19 12:48:36 -0500

    Create outgolf.py

---
+++ outgolf.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# outgolf.py
+#############
+# Outgolf ###
+# ckjbgames #
+# 2017 ######
+#############
+try:
+    import sys,os,re,math,string,random
+except ImportError:
+    print 'There was an error importing a module.'
+    print 'Sorry about that.'
+    sys.exit()
