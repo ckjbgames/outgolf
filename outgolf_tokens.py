3bb3ee0708f57a123d620cf0eeffbaf7a82f7df5
ckjbgames <ckjbgames@gmail.com>
2017-03-15 11:39:20 -0400

    Update outgolf_tokens.py

--- outgolf_tokens.py
+++ outgolf_tokens.py
@@ -4,3 +4,11 @@
 # The tokens for the Outgolf compiler. #
 # ckjbgames 2017 #######################
 ########################################
+def tokenize(code):
+    """
+    Tokenize the code.
+    """
+    tokens=[]
+    for currentChar in code:
+        if currentChar == '(':
+            tokens.append({'paren' : 
